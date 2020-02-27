94441a74501439e13748d0791107198aca28ca5c
Jake Louden <jakelouden@ORRHS.ORG>
2020-02-27 12:33:48 -0500

    Various things

---
+++ 54 String Methods/app.js
@@ -0,0 +1,34 @@
+let s
+let string = 'my name is Jesus               '
+
+console.log(string.length)
+
+//conversion
+
+let num = 10
+num = num.toString()
+
+//seraching
+
+s = string.search('J')
+
+s = string.startsWith("j")
+
+s = string.includes('e')
+
+//modifying
+
+s = string.toLowerCase()
+
+s = string.toUpperCase()
+
+s = string.concat(" and you will die")
+
+s = string.slice(5, 13)
+
+s = string.trim()
+
+num = Number(num)
+
+console.log(s)
+console.log(typeof num)
