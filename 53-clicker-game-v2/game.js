4c5860c3de3a8114fc3d1ea847d0083b3c542411
Jake Louden <jakelouden@ORRHS.ORG>
2020-02-27 11:57:20 -0500

    Started creating the game

--- 53-clicker-game-v2/game.js
+++ 53-clicker-game-v2/game.js
@@ -6,6 +6,7 @@ let difficultySlider
 let title
 
 // Initialize Game Variables
+let difficulty = 1
 let lives = 3
 let score = 0
 let winningNum = 5
@@ -21,12 +22,22 @@ function setup() {
 
 
   // Creates Difficulty Slider
-  const difficultyLabel = createElement('h4', 'DIFFICULTY')
+  difficultyLabel = createElement('h4', 'DIFFICULTY: ' + difficulty)
+  difficultyLabel.position(width / 20, height / 11)
+  difficultySlider = createSlider(1, 10, 1)
+  difficultySlider.position(width / 20, height / 7)
+  scoreDisplay = createElement('h4', 'SCORE: ' + score)
+  scoreDisplay.position(width / 20, height / 18)
 
+  bananaImg = createImg('banana.png', "Banana")
+  bananaImg.size(200,200)
 }
 
 function draw() {
   // Adjust frameRate according to slider
+  difficultyLabel.innerHTML = 'DIFFICULTY: ' + str(difficulty)
+  difficulty = difficultySlider.value()
+  frameRate(difficulty*3)
 
 
   // Randomly Position Banana
@@ -39,21 +50,32 @@ function mousePressed() {
   if (dist(mouseX, mouseY, bananaImg.x, bananaImg.y) > 200) {
     decreaseLives()
   }
+  if (dist(mouseX, mouseY, bananaImg.x, bananaImg.y) <= 200) {
+    increaseScore()
+  }
+  // else (){
+  //   increaseScore()
+  // }
 }
 
 function increaseScore() {
-
+  score += 1
+  checkWin()
 }
 
 function decreaseLives() {
-
+  lives-= 1
+  checkLose()
 }
 
 function checkWin() {
-
-
+  if (score === 5){
+    window.location.href = "win.html"
+  }
 }
 
 function checkLose() {
-
+  if (lives === 0){
+    window.location.href = "lose.html"
+  }
 }
